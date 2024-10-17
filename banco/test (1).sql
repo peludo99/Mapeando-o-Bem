0dce73eb2f8504d9f1ef38c761e08027694e23ae
CauandoP <148398651+peludo99@users.noreply.github.com>
2024-10-17 13:48:29 -0300

    a

--- banco/test (1).sql
+++ banco/test (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17/10/2024 às 16:50
+-- Tempo de geração: 17/10/2024 às 18:47
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
